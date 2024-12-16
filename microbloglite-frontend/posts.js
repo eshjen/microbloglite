363b6faba17f8282f7beb791ce06c416a1165d92
Eshei Jenkins <eshjenk@gmail.com>
2024-12-16 15:09:22 -0500

    get posts function

--- microbloglite-frontend/posts.js
+++ microbloglite-frontend/posts.js
@@ -1,3 +1,28 @@
 /* Posts Page JavaScript */
 
 "use strict";
+
+async function getPosts() {
+    
+
+const loginData = getLoginData();
+  try {
+    let promise = fetch(apiBaseURL + "/api/posts", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${loginData.token}`,
+      },
+    });
+    let response = await promise;
+    console.log(response)
+    let data = await response.json();
+    console.log(data);
+    createPostCards(data);
+  } catch (error) {
+    console.error;
+  }
+}
+  getPosts()
+
+  
